eb171b877ae2c03072a4e7080dd15f9a89c5da13
Varun Kharkwal <varunkh12345@gmail.com>
2023-10-15 12:58:30 +0530

    HomePage video fullscreen

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 const Header = () => {
+  const glogo="https://th.bing.com/th/id/R.a41d23250be5b1cfa015aab47ac8e5ae?rik=Wb6nyP4jZ1e65A&riu=http%3a%2f%2f2021.ictsci.in%2fimages%2fclients%2fgraphic-era.png&ehk=hFqvwBMhMQ6x9iG%2f0w1CWdIOVDRn6gtg0a%2bTb%2fRoJzA%3d&risl=&pid=ImgRaw&r=0";
   return (
-    <div className='h-20'>Header</div>
+    <header className='h-24 flex w-full justify-center items-center relative z-10'>
+      <img className='p-2 h-[inherit] absolute left-0' src={glogo} alt="" />
+      <nav>
+        <ul className='flex gap-[20vw] text-xl'>
+          <li className='texts'>Events</li>
+          <li className='texts'>About</li>
+          <li className='texts'>Register</li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
---
+++ components/LateralScroll.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const LateralScroll = () => {
+  return (
+    <section className="my-20 px-10 w-full flex justify-left items-center h-[450px] gap-20 overflow-x-scroll">
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+        <div className="circle"><div className="w-52 h-64 rounded-t-full overflow-hidden"><img src="/images/pp.png" alt="" className="w-full h-full" /></div> <h4 className='w-full text-center font-bold text-lg mt-10'>Varun Kharkwal</h4><p className='w-full text-center'>Web-Developer</p></div>
+      </section>
+  )
+}
+
+export default LateralScroll;
