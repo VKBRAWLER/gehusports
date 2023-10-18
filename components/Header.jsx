a132b1b75dc6b2d2fe06ad95cfc29ccc4c5fff69
Varun Kharkwal <varunkh12345@gmail.com>
2023-10-18 21:00:38 +0530

    image changed with responsive homepage and header

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,15 +3,17 @@ import Link from 'next/link';
 const Header = () => {
   const glogo="https://th.bing.com/th/id/R.a41d23250be5b1cfa015aab47ac8e5ae?rik=Wb6nyP4jZ1e65A&riu=http%3a%2f%2f2021.ictsci.in%2fimages%2fclients%2fgraphic-era.png&ehk=hFqvwBMhMQ6x9iG%2f0w1CWdIOVDRn6gtg0a%2bTb%2fRoJzA%3d&risl=&pid=ImgRaw&r=0";
   return (
-    <header className='h-24 flex w-full justify-center items-center relative z-10'>
-      <img className='p-2 h-[inherit] absolute left-0' src={glogo} alt="" />
+    <header className='cusgrid h-24 relative z-10'>
+      <img className='p-2' src={'/gs_logo.png'} alt="" />
       <nav>
-        <ul className='flex gap-[20vw] text-xl'>
-          <Link href={'/events'} className='texts'>Events</Link>
-          <li className='texts'>About</li>
-          <Link href={'/register'}  className='texts'>Register</Link>
+        <ul className='flex justify-evenly items-center h-full text-3xl'>
+            <Link className href='/'>Home</Link>
+            <Link className href='/about'>About</Link>
+            <Link className href='/contact'>Contact</Link>
         </ul>
       </nav>
+      <img className='p-2' src={glogo} alt="" />
+      
     </header>
   )
 }
