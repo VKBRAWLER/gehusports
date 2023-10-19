f7ebc8851e9d6e237f93325b39fe384e7d006e53
Varun Kharkwal <varunkh12345@gmail.com>
2023-10-19 12:56:34 +0530

    test #11

--- components/LateralScroll.jsx
+++ components/LateralScroll.jsx
@@ -1,19 +1,70 @@
 import React from 'react';
 
 const LateralScroll = () => {
-  const divs = [];
-  for (let i = 0; i < 10; i++) {
-    divs.push(<div className={"rounded-t-3xl bg-white bg-opacity-50 border-4 w-44 mem"}>
-    <img src="/images/pp.png" alt="" className={'rounded-full'}/>
-    <h4 className={'text-center font-bold text-lg mt-10'}>Varun Kharkwal</h4>
-    <p className={'text-center'}>Web-Developer</p>
-    </div>);
-  }
   return (
     <section className="my-20 px-10 flex justify-center h-80 gap-10 overflow-x-scroll flex-wrap flex-col">
-      {divs}
-    </section>
-  );
-};
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+        <div className="rounded-t-3xl bg-blue-200 w-40">
+        <img src="/images/pp.png" alt="" className='rounded-full'/>
+        <h4 className='text-center font-bold text-lg mt-10'>Varun Kharkwal</h4>
+        <p className='text-center'>Web-Developer</p>
+        </div>
+      </section>
+  )
+}
 
 export default LateralScroll;
