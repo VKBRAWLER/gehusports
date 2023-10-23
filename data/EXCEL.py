43a11d5ca5452d47671eac2a5b9da950552d3de0
Varun Kharkwal <varunkh12345@gmail.com>
2023-10-23 23:34:33 +0530

    register.json added to database

--- data/EXCEL.py
+++ data/EXCEL.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import json
 # Read the Excel file
-df = pd.read_excel('data/excel/events.xlsx')
+df1 = pd.read_excel('data/excel/events.xlsx')
+df2 = pd.read_excel('data/excel/register.xlsx')
 
 # Convert the data to a dictionary
-data = df.to_dict(orient='records')
+data1 = df1.to_dict(orient='records')
+data2 = df2.to_dict(orient='records')
 
 # Write the data to a JSON file
 with open('data/json/events.json', 'w') as f:
-    json.dump(data, f, indent=4)
+    json.dump(data1, f, indent=4)
+with open('data/json/register.json', 'w') as f:
+    json.dump(data2, f, indent=4)
