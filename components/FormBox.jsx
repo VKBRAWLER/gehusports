89153564b12008f3e929835765970ed40eb571e0
Varun Kharkwal <varunkh12345@gmail.com>
2023-10-25 21:06:08 +0530

    InsertImage glitch fixed

--- components/FormBox.jsx
+++ components/FormBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 const FormBox = (params) => {
   if (params.parameter === 'Screenshot of Payment') {
     return <FormImage parameter={params.parameter} />
@@ -47,22 +47,31 @@ const FormCollege = (params) => {
 }
 
 const FormImage = (params) => {
+  const [SSinput, setSSinput] = useState(null);
   const [screenshot, setScreenshot] = useState(null);
-    const handleScreenshotChange = (event) => {
+  const handleScreenshotChange = (event) => {
     setScreenshot(event.target.files[0]);
+    SSinput.value = null;
   };
+  const handleScreenshotClick = () => {
+    SSinput.click();
+    setScreenshot(null);
+  }
+  useEffect(()=> {
+    setSSinput(document.getElementById('screenshot'));
+  })
   return (
   <div className='FormDiv'>
     <label className='FormLabel' htmlFor="screenshot">{params.parameter}:</label>
     <input className='form-box sr-only' type="file" id="screenshot" accept="image/*" onChange={handleScreenshotChange} required />
     <div className='w-full m-2'>
       {screenshot
-      ?<>
-      <img className='w-full' src={URL.createObjectURL(screenshot)} alt="Screenshot" />
-      <div onClick={()=>{document.getElementById('screenshot').click()}} className='cursor-pointer hover:underline float-left'>Change Image</div>
-      <div onClick={()=> {setScreenshot(null)}} className='cursor-pointer hover:underline float-right'>Remove Image</div>
+      ?<> 
+      <div className="w-full flex justify-center"><img className='h-4/5 w-3/5' src={URL.createObjectURL(screenshot)} alt="Screenshot" /></div>
+      <div onClick={()=>{handleScreenshotClick()}} className='cursor-pointer md:text-xl hover:underline float-left'>Change Image</div>
+      <div onClick={()=> {setScreenshot(null);}} className='cursor-pointer md:text-xl hover:underline float-right'>Remove Image</div>
       </>:
-      <div className='flex justify-center w-full'><span onClick={()=>{document.getElementById('screenshot').click()}} className="cursor-pointer form-box px-4 py-1 rounded text-white border-2 border-white bg-yellow-300">Click Here To Upload</span></div>
+      <div className='flex justify-center w-full'><span onClick={()=>{handleScreenshotClick()}} className="cursor-pointer form-box px-4 py-1 rounded text-white border-2 border-white bg-yellow-300">Click Here To Upload</span></div>
       }
     </div>
   </div>
