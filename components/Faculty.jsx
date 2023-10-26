d66abc1657c6c77bc16c3678af985042fc0c3d98
Varun Kharkwal <varunkh12345@gmail.com>
2023-10-26 11:23:30 +0530

    Responsiveness fixed for events page and faculty

--- components/Faculty.jsx
+++ components/Faculty.jsx
@@ -4,24 +4,30 @@ const Faculty = () => {
   const facimg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEwfjJEZno_M_rTutJHcl9BTTb8wIHfXZ2z3iNjmvZ&s";
   const dicimg = 'https://cdn.discordapp.com/attachments/1163848747504124019/1164445807332831272/mc.jpg?ex=65433daa&is=6530c8aa&hm=02f30612bb298b5cf75fa14c2553e13b99f94793547b009976b2f8016848098e&';
   return (
-    <>
-    <h1 className='w-full text-center font-extrabold text-5xl texts relative bg-gradient-to-b from-white pt-20'>FACULTY INCHARHGE</h1>
-      <section className="my-10 w-full flex justify-evenly items-center min-h-[600px] flex-wrap">
-        <div className="boss relative order-n group overflow-hidden">
-          <p className='text-white absolute -bottom-44 p-2 group-hover:bottom-14 duration-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi a aliquid explicabo eaque? Vero dignissimos officia nulla laudantium id nesciunt laboriosam facere adipisci amet. Ducimus, deleniti. Ea provident perspiciatis earum.</p>
-          <img src={dicimg} alt="" className="lg:w-full h-full max-w-full" />
-          <h4 className='w-full text-center bottom-3 absolute text-3xl font-semibold bg-white'>Dr. M. C. Lohani</h4>
-        </div>
-        <div className="boss relative order-2">
-          <img src={facimg} alt="" className="lg:w-full h-full max-w-full" />
-          <h4 className='w-full text-center bottom-3 absolute text-3xl font-semibold bg-white'>Faculty 1</h4>
-        </div>
-        <div className="boss relative order-4">
-          <img src={facimg} alt="" className="lg:w-full h-full max-w-full" />
-          <h4 className='w-full text-center bottom-3 absolute text-3xl font-semibold bg-white'>Faculty 2</h4>
-        </div>
-      </section>
-    </>
+  <>
+  <h1 className='w-full text-center font-extrabold text-3xl sm:text-5xl texts relative bg-gradient-to-b from-white py-10 md:py-20'>FACULTY INCHARHGE</h1>
+    <section className="flex justify-evenly flex-wrap mx-5 gap-5">
+      <div className="max-w-sm sm:w-72 md:w-1/4 bg-white">
+        <img className='w-full' src={facimg} alt="" />
+        <div className=''>
+          <h3 className='text-2xl bg-blue-400 w-full text-center'>Faculty Name 01</h3>
+          <p className='m-2'>``Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi quibusdam et tempore, aut recusandae aspernatur asperiores. Possimus, accusamus, dicta cum molestias quisquam placeat provident, veritatis sapiente assumenda necessitatibus libero quae.``</p></div>
+      </div>
+      <div className="max-w-sm sm:w-72 md:w-1/4 bg-white">
+        <img className='w-full' src={facimg} alt="" />
+        <div className=''>
+          <h3 className='text-2xl bg-blue-400 w-full text-center'>Faculty Name 01</h3>
+          <p className='m-2'>``Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi quibusdam et tempore, aut recusandae aspernatur asperiores. Possimus, accusamus, dicta cum molestias quisquam placeat provident, veritatis sapiente assumenda necessitatibus libero quae.``</p></div>
+      </div>
+      <div className="max-w-sm sm:w-72 md:w-1/4 bg-white">
+        <img className='w-full' src={facimg} alt="" />
+        <div className=''>
+          <h3 className='text-2xl bg-blue-400 w-full text-center'>Faculty Name 01</h3>
+          <p className='m-2'>``Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi quibusdam et tempore, aut recusandae aspernatur asperiores. Possimus, accusamus, dicta cum molestias quisquam placeat provident, veritatis sapiente assumenda necessitatibus libero quae.``</p></div>
+      </div>
+    </section>
+    
+  </>
   )
 }
 
