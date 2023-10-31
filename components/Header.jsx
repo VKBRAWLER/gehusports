f01c39dec2667ab44c141a3eebdb2cbbd43ec52a
Varun Kharkwal <varunkh12345@gmail.com>
2023-11-01 02:09:08 +0530

    Umang EventPage Modified

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <ul className='flex justify-evenly items-center h-full text-md sm:text-3xl'>
             <li><Link className='text-white bg-black rounded-md bg-opacity-75 p-1 sm:p-2 lg:hover:text-black lg:hover:bg-white lg:hover:bg-opacity-75' href='/'>Home</Link></li>
             <li><Link className='text-white bg-black rounded-md bg-opacity-75 p-1 sm:p-2 lg:hover:text-black lg:hover:bg-white lg:hover:bg-opacity-75' href='/events'>Event</Link></li>
-            <li><Link className='text-white bg-black rounded-md bg-opacity-75 p-1 sm:p-2 lg:hover:text-black lg:hover:bg-white lg:hover:bg-opacity-75' href='/register'>About</Link></li>
+            {/* <li><Link className='text-white bg-black rounded-md bg-opacity-75 p-1 sm:p-2 lg:hover:text-black lg:hover:bg-white lg:hover:bg-opacity-75' href='/register'>About</Link></li> */}
         </ul>
       </nav>
       <img className='p-2' src={gehulogo} alt="" />
