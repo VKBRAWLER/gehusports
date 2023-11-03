0a8647c4dec7fe27848eaa20a536ebafe5e41490
Varun Kharkwal <varunkh12345@gmail.com>
2023-11-03 18:30:34 +0530

    formatted remaining non rendering files

--- temp/LateralScroll.jsx
+++ temp/LateralScroll.jsx
@@ -6,12 +6,12 @@ const LateralScroll = () => {
   const divs = [];
   for (let i = 0; i < 10; i++) {
     divs.push(<div className={"rounded-t-3xl bg-white bg-opacity-50 border-4 w-44 mem"}>
-    {(i%2==0)?
-    <img src={akkida} alt="" className={'rounded-full w-44 h-44'} />:
-    <img src={amitda} alt="" className={'rounded-full w-44 h-44'} />
-    }
-    <h4 className={'text-center font-bold text-lg mt-10'}>Varun Kharkwal</h4>
-    <p className={'text-center'}>Web-Developer</p>
+      {(i % 2 == 0) ?
+        <img src={akkida} alt="" className={'rounded-full w-44 h-44'} /> :
+        <img src={amitda} alt="" className={'rounded-full w-44 h-44'} />
+      }
+      <h4 className={'text-center font-bold text-lg mt-10'}>Varun Kharkwal</h4>
+      <p className={'text-center'}>Web-Developer</p>
     </div>);
   }
   return (
