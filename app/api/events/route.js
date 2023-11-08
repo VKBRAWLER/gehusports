b7ea5f35d7342dd037d0cb15fda788766e047962
Varun Kharkwal <varunkh12345@gmail.com>
2023-11-08 13:18:50 +0530

    Refactor event creation and database connection

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -3,10 +3,9 @@ import { ConnectToDB } from "@utils/database";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    let events = { event_code, title, start_date, end_date, poster_image, bg_image }
-    events = await request.json();
+    let { _id, title, start_date, end_date, poster_image, bg_image, created_by } = await request.json();
     await ConnectToDB();
-    await Events.create(events);
+    await Events.create({ _id, title, start_date, end_date, poster_image, bg_image, created_by });
     return NextResponse.json({ message: "Event created successfully"}, {status: 200});
 }
 
--- utils/database.js
+++ utils/database.js
@@ -8,7 +8,6 @@ export const ConnectToDB = async () => {
     }
     try {
         await mongoose.connect(process.env.MONGO_URI , {
-            useNewUrlParser: true,
             dbName: process.env.MONGO_DB_NAME,
         });
         isConnected = true;
