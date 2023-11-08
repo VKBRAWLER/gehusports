05e52e91e3c968d1318e57f19cd8559162e44bcb
Varun Kharkwal <varunkh12345@gmail.com>
2023-11-08 13:18:12 +0530

    Refactor events API and model schema

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -1,9 +1,17 @@
 import Events from "@models/events";
 import { ConnectToDB } from "@utils/database";
 import { NextResponse } from "next/server";
+
 export async function POST(request) {
-    const { event_code, title, start_date, end_date, poster_image, bg_image } = await request.json();
+    let events = { event_code, title, start_date, end_date, poster_image, bg_image }
+    events = await request.json();
     await ConnectToDB();
-    await Events.create({ event_code, title, start_date, end_date, poster_image, bg_image });
+    await Events.create(events);
     return NextResponse.json({ message: "Event created successfully"}, {status: 200});
+}
+
+export async function GET(request) {
+    await ConnectToDB();
+    const events = await Events.find({}); // find all events
+    return NextResponse.json(events, {status: 200}); // return the response
 }
--- models/events.js
+++ models/events.js
@@ -1,25 +1,50 @@
 import { Schema, model, models } from "mongoose";
 
 const EventsSchema = new Schema({
-    event_code: {
+    _id: {
         type: String,
+        require: true,
     },
     title: {
         type: String,
+        require: true,
     },
     start_date: {
-        type: String,
+        type: Date,
+        default: null,
     },
     end_date: {
-        type: String,
+        type: Date,
+        default: null,
     },
     poster_image: {
         type: String,
+        default: '',
     },
     bg_image: {
         type: String,
+        default: '',    
     },
-})
+    visible: {
+        type: Boolean,
+        default: false,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    created_by: {
+        type: String,
+        default: 'admin',
+        require: true,
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+    },
+
+},
+    { versionKey: false });
 
 const Events = models.Events || model('Events', EventsSchema);
 
