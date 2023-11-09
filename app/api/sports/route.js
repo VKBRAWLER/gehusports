6f856b63478fd09565a9b0947ff488f8cbe6f060
Varun Kharkwal <varunkh12345@gmail.com>
2023-11-09 16:55:11 +0530

    Add CRUD operations for Sports model

---
+++ app/api/sports/route.js
@@ -0,0 +1,22 @@
+import Sports from "@models/sports";
+import { ConnectToDB } from "@utils/database";
+import { NextResponse } from "next/server";
+
+export async function POST (request) {
+    let { _id, title, event_code, start_date, end_date, poster_image, Register } = await request.json();
+    await ConnectToDB();
+    await Sports.create({ _id, title, event_code, start_date, end_date, poster_image, Register });
+    return NextResponse.json({ message: "Sport created successfully"}, {status: 200});
+}
+export async function GET(request) {
+    await ConnectToDB();
+    const sports = await Sports.find({});
+    return NextResponse.json(sports, {status: 200});
+}
+
+export async function DELETE(request) {
+    const { _id } = await request.json();
+    await ConnectToDB();
+    await Sports.deleteOne({ _id });
+    return NextResponse.json({ message: "Sport deleted successfully" }, { status: 200 });
+}
---
+++ models/sports.js
@@ -0,0 +1,42 @@
+import { Schema, model, models } from "mongoose";
+
+const SportsSchema = new Schema({
+    _id: {
+        type: String,
+        unique: [true, 'id is already exists'],
+        require: [true, 'id is required'],
+    },
+    title: {
+        type: String,
+        require: [true, 'title is required'],
+    },
+    event_code: {
+        type: String,
+        require: [true, 'event_code is required'],
+    },
+    start_date: {
+        type: Date,
+        default: null,
+    },
+    end_date: {
+        type: Date,
+        default: null,
+    },
+    poster_image: {
+        type: String,
+        default: null,
+    },
+    register: {
+        type: Boolean,
+        default: false,
+    },
+    status: {
+        type: Array,
+        default: [],
+    }
+},
+    { versionKey: false });
+
+const Sports = models.Sports || model('Sports', SportsSchema);
+
+export default Sports;
