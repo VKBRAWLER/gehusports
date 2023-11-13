88d98a0a86b6256b1cb8379ab5176b8534d92c9f
Varun Kharkwal <varunkh12345@gmail.com>
2023-11-13 10:49:17 +0530

    Added bcrypt for password hashing and user signIn

--- app/admin/signin/page.jsx
+++ app/admin/signin/page.jsx
@@ -16,9 +16,41 @@ const SignInPage = () => {
   useEffect(() => {
     setUpProviders();
   }, [session])
-  const handleSubmit = (event) => {
+  const signInCred = async () => {
+    const response = await fetch('/api/users/signin', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({email, password})
+    })
+    const data = await response.json();
+    if (data.error) {
+      alert(data.error);
+    }
+    else {
+      alert('Sign up successful');
+      router.push('/admin');
+    }
+  }
+  const checkForm = () => {
+    console.log({email, password});
+    if (email === '') {
+      alert('Email is required');
+    }
+    else if (email.indexOf('@') === -1 || email.indexOf('.') === -1) {
+      alert('Email is invalid');
+    }
+    else if (password === '') {
+      alert('Password is required');
+    }
+    else {
+      signInCred();
+    }
+  }
+  const SubmitEvent = (event) => {
     event.preventDefault();
-    alert("Done");
+    checkForm();
   };
   if (!session) {
   return (
@@ -28,16 +60,13 @@ const SignInPage = () => {
         <form>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
-            <input type="email" id="email" name="email" className="w-full border rounded py-2 px-3" />
+            <input type="email" id="email" className="w-full border rounded py-2 px-3" onChange={(e)=>{setEmail(e.target.value)}} />
           </div>
           <div className="mb-4">
             <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-            <input type="password" id="password" name="password" className="w-full border rounded py-2 px-3" />
+            <input type="password" id="password" className="w-full border rounded py-2 px-3" onChange={(e)=>{setPassword(e.target.value)}} />
           </div>
-          <div className="mb-4 text-right">
-            <a href="#" className="text-blue-500 text-sm">Forget Password?</a>
-          </div>
-          <button type="submit" className="w-full bg-blue-500 text-white py-2 px-4 rounded">Sign In</button>
+          <button onClick={(e)=>{SubmitEvent(e)}} className="w-full bg-blue-500 text-white py-2 px-4 rounded">Sign In</button>
         </form>
         <div className="mt-4 text-center">
           <p className="text-gray-500 text-sm">Or sign up with</p>
@@ -48,7 +77,7 @@ const SignInPage = () => {
           })}
         </div>
         <div className="mt-4 text-center">
-          <Link href={'/admin/signup'} className="text-gray-500 text-sm">Already a user? <span className="text-blue-500">Sign Up</span></Link>
+          <Link href={'/admin/signup'} className="text-gray-500 text-sm">New in Here? <span className="text-blue-500">Sign Up</span></Link>
         </div>
       </div>
     </main>
@@ -56,6 +85,9 @@ const SignInPage = () => {
         }
   else {
     router.push('/admin');
+    return (
+      <div></div>
+    )
   }
 };
 
