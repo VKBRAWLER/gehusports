2eef94cf3c8c2954545840e70dd62366d543b94f
Varun Kharkwal <varunkh12345@gmail.com>
2023-11-17 10:57:52 +0530

    Added Verified component in Auth.jsx

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,5 +1,5 @@
 import AdminWelcome from "@components/AdminWelcome";
-import Auth from "@pages/Auth";
+import { Auth } from "@pages/Auth";
 const AdminPage = () => {
   return (
     <Auth>
--- pages/Auth.jsx
+++ pages/Auth.jsx
@@ -2,7 +2,7 @@
 import Loading from "@app/loading";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-function Auth({ children }) {
+export function Auth({ children }) {
   const { status } = useSession();
   if (status === "authenticated") {
     return (
@@ -22,5 +22,23 @@ function Auth({ children }) {
     <Loading />
   );
 }
+export function Verified ({ children }) {
+  const { data: session } = useSession();
+  if (session?.user.is_verfied) {
+    return (
+      <>{children}</>
+    );
+  }
+  if (!session?.user.is_verfied) {
+    return (
+      <main>
+        <h1 className="text-center text-3xl">You must be verified to access this page</h1>
+        <Link href={'/admin/profile'} className="text-center text-3xl bg-blue-300 p-3 rounded-xl">Verify</Link>
+      </main>
+    );
+  }
 
-export default Auth;
+  return (
+    <Loading />
+  );
+}
