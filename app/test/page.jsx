0665c701e6f1fb152d61e75a38833203054e7e9b
Varun Kharkwal <varunkh12345@gmail.com>
2023-11-17 14:32:00 +0530

    test #33

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,28 +1,10 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-
-async function importion() {
-    const res = await fetch('https://valorant-api.com/v1/agents/');
-    const data = await res.json();
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    return await data.data;
-}
-
-const TestPage = async () => {
-    const events = await importion()
+import { useEffect, useRef } from "react";
+const TestPage = () => {
+    const spanRef = useRef(null);
     return (
-        <>
-            <div className='w-full text-center uppercase text-5xl'>TestPage</div>
-            {events.map((event, index) => {
-                return (
-                    <div key={index} className='w-full text-white uppercase text-5xl'>{index + 1}. {event.displayName}
-                        <img src={event.displayIcon} alt="" />
-                    </div>
-                )
-            })
-            }
+        <><input ref={spanRef} />
         </>
     )
-}
-
+};
 export default TestPage;
