3863f538d9147e7ce8a09e21511384f7e8d146e4
Varun Kharkwal <varunkh12345@gmail.com>
2023-11-19 09:12:20 +0530

    Add authentication components in userlist

--- app/admin/userlist/page.jsx
+++ app/admin/userlist/page.jsx
@@ -1,4 +1,6 @@
 "use client";
+import Auth from "@components/Auth";
+import RoleAuth from "@components/RoleAuth";
 import Verified from "@components/Verified";
 import { useEffect, useState } from "react";
 const UserListPage = () => {
@@ -17,19 +19,23 @@ const UserListPage = () => {
     getUsers();
   }, []);
   return (
-    <Verified>
-      <main>
-          <section className="flex flex-col gap-5 p-5">
-          { userList?.length ?
-          userList?.map((user) => (
-            <div key={user._id.toString()} className="w-full bg-white h-32 rounded-2xl flex justify-between items-center p-2"><img src={user.image} alt="" className="rounded-full" /> <p>{user.email}</p><span>Role: {user.role}</span><span>{user.is_verfied? <>verified</>: <>not verified</>}</span></div>
-          ))
-          : <p>No users found</p>  
-          }
-          </section>
-      </main>
-    </Verified>
-    )
+    <Auth>
+      <Verified>
+        <RoleAuth role={['developer', 'teacher', 'organising member']}>
+          <main>
+            <section className="flex flex-col gap-5 p-5">
+              {userList?.length ?
+              userList?.map((user) => (
+                  <div key={user._id.toString()} className="w-full bg-white h-32 rounded-2xl flex justify-between items-center p-2"><img src={user.image} alt="" className="rounded-full" /> <p>{user.email}</p><span>Role: {user.role}</span><span>{user.is_verfied ? <>verified</> : <>not verified</>}</span></div>
+                ))
+                : <p>No users found</p>
+              }
+            </section>
+          </main>
+        </RoleAuth>
+      </Verified>
+    </Auth>
+  )
 }
 
 export default UserListPage;
