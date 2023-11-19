b64f9ac7c5d3d3622794484b84b6f2a9198b124f
Varun Kharkwal <varunkh12345@gmail.com>
2023-11-19 18:14:50 +0530

    Format Document #2

--- app/admin/profile/page.jsx
+++ app/admin/profile/page.jsx
@@ -10,24 +10,24 @@ const ProfilePage = () => {
   return (
     <Auth>
       <main className="flex flex-col items-center justify-center bg-white">
-          <div className="relative w-40 h-40 rounded-full overflow-hidden">
-            <Image
-              src={session?.user.image}
-              alt="Profile picture"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <div>
-            <h1 className="text-2xl font-bold mt-4">{session?.user.name}</h1>
-            <p className="text-gray-500">{session?.user.email}</p>
-            <p className="text-gray-500">{session?.user.role}</p>
-            <p className="text-gray-500">
-              {session?.user.is_verfied ? "Verified" : "Not verified"}
-            </p>
-          </div>
-          {session?.user.is_verfied || 
-          <ProfileDetailsForm user={session?.user}/>}
+        <div className="relative w-40 h-40 rounded-full overflow-hidden">
+          <Image
+            src={session?.user.image}
+            alt="Profile picture"
+            layout="fill"
+            objectFit="cover"
+          />  
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold mt-4">{session?.user.name}</h1>
+          <p className="text-gray-500">{session?.user.email}</p>
+          <p className="text-gray-500">{session?.user.role}</p>
+          <p className="text-gray-500">
+            {session?.user.is_verfied ? "Verified" : "Not verified"}
+          </p>
+        </div>
+        {session?.user.is_verfied ||
+          <ProfileDetailsForm user={session?.user} />}
       </main>
     </Auth>
   );
