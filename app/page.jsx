4827dfde20b7211279b3b4f0f62423328a3a2ca6
Varun Kharkwal <varunkh12345@gmail.com>
2024-03-17 20:38:07 +0530

    Added new font and changed UI

--- app/page.jsx
+++ app/page.jsx
@@ -2,18 +2,22 @@ import LateralScroll from '@components/LateralScroll';
 import SportsTypeBox from '@components/SportsTypeBox';
 import EnduranceCup from '@components/EnduranceCup';
 import Faculty from '@components/Faculty';
+import Director from '@components/Director';
+import Footer from '@components/Footer';
 const HomePage = () => {
   return (
     <>
       <EnduranceCup />
       <main className='z-10 relative'>
         <div className="w-full py-10 mt-[calc(60vh-6rem)] bg-white overflow-hidden">
-          <SportsTypeBox />
+          {/* <SportsTypeBox /> */}
+        <Director />
         </div>
         <Faculty />
-        <LateralScroll />
+        {/* <LateralScroll /> */}
         <div className="h-24"></div>
       </main>
+      <Footer />
     </>
   )
 }
